54e6a9f08d6610ce10e9085747a9050f3eefeeac
Abhishek <abhishekmandha2018@gmail.com>
2023-08-09 01:17:04 +0530

    porfolio modification

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import portfolioproject from '../assets/Portfolio/portfolioproject.jpeg'
+import expesetracker from '../assets/Portfolio/expensetracker.png'
+import todoapp from '../assets/Portfolio/todoapp.png'
 
 
 const Portfolio=() => {
-    const  url = 'https://earnest-kitsune-b4796e.netlify.app'
+    const portfolios =[
+        {
+            id:1,
+            src:expesetracker
+        },
+        {
+            id:2,
+            src:todoapp
+        },
+        {
+            id:3,
+            src:portfolioproject
+        }
+
+    ]
   return (
     <div name='portfolio' className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
@@ -12,13 +28,18 @@ const Portfolio=() => {
                 <p className='py-6'>Check out some of my work right here</p>
             </div>
 
+
+            
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-                <div className='shadow-md shadow-gray-600 rounded-lg'>
-                    <img src={portfolioproject} alt="portfolio project" className='rounded-md duration-200 hover:scale-105'/>
+            {portfolios.map(({id,src})=>(
+                <div key={id}className='shadow-md shadow-gray-600 rounded-lg'>
+                    <img src={src} alt="portfolio project" className='rounded-md duration-200 hover:scale-105'/>
                     <div className='flex items-center justify-center'>
-                        <button onClick={() => { window.location.href = url; } } className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+                        <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'></button>
                     </div>
+            
                 </div>
+            ))}
             </div>
         </div>
     </div>
